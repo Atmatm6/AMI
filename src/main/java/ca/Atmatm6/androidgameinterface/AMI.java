dbe804660577431b664df132687ec4f4255edc56
Atmatm6 Yolo <atmatm6000@gmail.com>
2015-03-06 14:29:46 -0600

    Etc

---
+++ src/main/java/ca/Atmatm6/androidgameinterface/AMI.java
@@ -0,0 +1,7 @@
+package ca.Atmatm6.androidgameinterface;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.modid, name = Reference.name, version = Reference.vers)
+public class AMI {
+}
---
+++ src/main/java/ca/Atmatm6/androidgameinterface/Reference.java
@@ -0,0 +1,7 @@
+package ca.Atmatm6.androidgameinterface;
+
+public class Reference {
+    public static final String modid = "ami";
+    public static final String name = "Android Minecraft Interface";
+    public static final String vers = "0.0.a";
+}
